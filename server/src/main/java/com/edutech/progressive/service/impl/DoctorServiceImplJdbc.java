df9f76a0c505f433fb634c948cab1e5cdaa8f28f
shivamkorade <dummy@gmail.com>
2026-02-23 12:37:06 +0000

    Day 1 Taks

--- server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/DoctorServiceImplJdbc.java
@@ -1,5 +1,35 @@
 package com.edutech.progressive.service.impl;
 
-public class DoctorServiceImplJdbc  {
+import java.util.List;
+
+import com.edutech.progressive.entity.Doctor;
+import com.edutech.progressive.service.DoctorService;
+
+public class DoctorServiceImplJdbc implements DoctorService {
+
+    @Override
+    public List<Doctor> getAllDoctors() {
+        return null;
+    }
+
+    @Override
+    public Integer addDoctor(Doctor doctor) {
+        return -1;
+    }
+
+    @Override
+    public List<Doctor> getDoctorSortedByExperience() {
+        return null;
+    }
+
+    public Doctor getDoctorById(int doctorId){
+        return null;
+    }
+    public void updateDoctor(Doctor doctor){
+        
+    }
+    public void deleteDoctor(int doctorId){
+        
+    }
 
 }
--- server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplArraylist.java
+++ server/src/main/java/com/edutech/progressive/service/impl/PatientServiceImplArraylist.java
@@ -1,5 +1,29 @@
 package com.edutech.progressive.service.impl;
 
-public class PatientServiceImplArraylist  {
+import java.util.List;
 
+import com.edutech.progressive.entity.Patient;
+import com.edutech.progressive.service.PatientService;
+
+public class PatientServiceImplArraylist implements PatientService {
+
+    @Override
+    public List<Patient> getAllPatients() {
+        return null;
+    }
+
+    @Override
+    public Integer addPatient(Patient patient) {
+        return -1;
+    }
+
+    @Override
+    public List<Patient> getAllPatientSortedByName() {
+        return null;
+    }
+
+    public void emptyArrayList(){
+        
+    }
+    
 }
