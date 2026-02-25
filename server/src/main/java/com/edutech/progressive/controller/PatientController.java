2107acdae229e32e4904cdf649f1b840a9299abc
subham <subhampatro11@gmail.com>
2026-02-25 11:14:24 +0000

    Day 4 task

--- server/src/main/java/com/edutech/progressive/controller/PatientController.java
+++ server/src/main/java/com/edutech/progressive/controller/PatientController.java
@@ -2,9 +2,10 @@ package com.edutech.progressive.controller;
 
 import com.edutech.progressive.entity.Patient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
 public class PatientController {
 
     public ResponseEntity<List<Patient>> getAllPatients() {
--- server/src/main/java/com/edutech/progressive/entity/Doctor.java
+++ server/src/main/java/com/edutech/progressive/entity/Doctor.java
@@ -1,6 +1,14 @@
 package com.edutech.progressive.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Doctor implements Comparable<Doctor>{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int doctorId;
     private String fullName;
     private String specialty;
